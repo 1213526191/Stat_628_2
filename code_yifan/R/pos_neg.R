c3d6f5fa9b83090db9ef88dd43d33ed9a07b6fec
1213526191 <ivanlee142857@gmail.com>
2018-03-10 10:48:54 -0600

    mybad

--- code_yifan/R/pos_neg.R
+++ code_yifan/R/pos_neg.R
@@ -4,89 +4,61 @@ library(Matrix)
 library(topicmodels)
 library(glmnet) 
 
-data_ori = read_csv("../../data/li_first100000forR.csv")
+data_ori = read_csv("../../data/random100000w.csv")
 
 data_df = data_frame(line = 1:nrow(data_ori),
                      text = data_ori$text, 
                      stars = data_ori$stars)
-
-data_df5 <- data_df %>%
-  filter(stars == 5)
-
-data_df1 <- data_df %>%
-  filter(stars == 1)
-
-
-myexpect = function(words, data, counts){
-  data_useful <- data %>%
-    filter(word == words) %>%
-    count(stars)
-  names(data_useful)[2] = "n"
-  my_star = tibble(stars = c(1:5), n = rep(0, 5))
-  data_useful2 <- rbind(data_useful, my_star) %>%
-    group_by(stars) %>%
-    summarise(n = max(n)) %>%
-    mutate(xx = n/counts) %>%
-    mutate(xx = xx/sum(xx))
-  if(is.na(sum(data_useful2$xx*c(1,2,3,4,5)))){
-    return(0)
-  }else{
-    return(sum(data_useful2$xx*c(1,2,3,4,5)))
-  }
-}
-
-myvar = function(words, data, counts){
-  data_useful <- data %>%
-    filter(word == words) %>%
-    count(stars)
-  names(data_useful)[2] = "n"
-  my_star = tibble(stars = c(1:5), n = rep(0, 5))
-  data_useful2 <- rbind(data_useful, my_star) %>%
-    group_by(stars) %>%
-    summarise(n = max(n)) %>%
-    mutate(xx = n/counts) %>%
-    mutate(xx = xx/sum(xx))
-  if(is.na(var(data_useful2$xx))){
-    return(0)
-  }else{
-    return(var(data_useful2$xx))
-  }
-}
-
-n_sh = 0.01*nrow(data_df)
 data_tidy <- data_df %>%
   unnest_tokens(word, text)
 
-major <- data_tidy %>%
-  count(word, sort = T) %>%
-  filter(n > n_sh)
+
+
 
 data_tidy2 <- data_tidy %>%
-  inner_join(major, by = "word")
-
-word_var <- data_frame(word = major$word, var = rep(0, nrow(major)), expect = rep(0, nrow(major)))
-data_tidy_count <- data_tidy2 %>%
-  count(stars)
-t1 = Sys.time()
-word_var$var <- apply(word_var[,1], 1, myvar, 
-                      data = data_tidy2, counts = data_tidy_count$nn)
-t2 = Sys.time()
-print(t2-t1)
-word_var$expect <- apply(word_var[,1], 1, myexpect, 
-                      data = data_tidy2, counts = data_tidy_count$nn)
-t3 = Sys.time()
-print(t3-t2)
-word_var <- word_var %>%
-  arrange(desc(var))
-filename = paste("../../data/pos_neg.csv", sep = "")
-write_csv(word_var, filename)
-
-n = 1
-data_tidy2 <- data_tidy %>%
-  filter(stars == 1)  %>%
+  distinct(line, word, stars) %>%
+  group_by(stars, word) %>%
+  summarise(count = n()) 
+data_tidy2_5 <- data_tidy2 %>%
   group_by(word) %>%
-  summarise(nnn = n()) %>%
-  arrange(desc(nnn))
+  summarise(count2_5 = sum(count)) %>%
+  filter(count2_5 > 50) 
+major <- data_tidy2_5 %>%
+  select(word)
+data_tidy3 <- data_tidy2 %>%
+  inner_join(major, by = "word") %>%
+  spread(stars, count, fill = 0)
 
-filename = paste("../../data/dist", as.character(n), "_ori.csv", sep = "")
-write_csv(data_tidy2, filename)
+star_count <- data_df %>%
+  group_by(stars) %>%
+  summarise(count = n())
+  
+mycount = star_count$count
+
+myprop = function(data_row, counts){
+  data2 = as.numeric(data_row[2:6])
+  xx = numeric(5)
+  for(i in 1:5){
+    if(counts[i] == 0){
+      xx[i] = 0
+    }else if(sum(data2[-i]) == 0){
+      xx[i] = 100
+    }else if(sum(counts[-i]) == 0){
+      xx[i] = 100
+    }else{
+      y1 = data2[i]/counts[i]
+      y2 = sum(data2[-i])/sum(counts[-i])
+      xx[i] = y1/y2
+    }
+  }
+  return(xx)
+}
+
+result = as.tibble(t(apply(data_tidy3, 1, myprop, counts = mycount)))
+result$word = data_tidy3$word
+result <- result %>%
+  select(word, V1:V5)
+
+
+filename = paste("../../data/feature4jianmin.cav")
+write_csv(result, filename)
